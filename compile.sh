eb3ae704c2914e45e16f22466e6f6558c19c8f12
Matteo Corralx Bertello <bertello.matteo@gmail.com>
2014-07-23 17:07:45 +0200

    Fixed compilation for Linux

--- compile.sh
+++ compile.sh
@@ -17,54 +17,50 @@ echo ""
 }
  
 while [ "$1" != "" ]; do
-	PARAM=$1
-	VALUE=$2
-	case $PARAM in
-		-h | --help)
-			usage
-			exit
-		;;
-		-c | --compiler)
-			compiler=$VALUE
-			shift
-		;;
-		-b | --build)
-			build=$VALUE
-			shift
-		;;
-		-j | --cores)
-			j=$VALUE
-			shift
-		;;
-		-s | --size)
-			size=$VALUE
-			shift
-		;;
-		*)
-			echo "ERROR: unknown parameter \"$PARAM\""
-			usage
-			exit 1
-		;;
-	esac
+        PARAM=$1
+        VALUE=$2
+        case $PARAM in
+                -h | --help)
+                        usage
+                        exit
+                ;;
+                -c | --compiler)
+                        compiler=$VALUE
+                        shift
+                ;;
+                -b | --build)
+                        build=$VALUE
+                        shift
+                ;;
+                -j | --cores)
+                        j=$VALUE
+                        shift
+                ;;
+                -s | --size)
+                        size=$VALUE
+                        shift
+                ;;
+                *)
+                        echo "ERROR: unknown parameter \"$PARAM\""
+                        usage
+                        exit 1
+                ;;
+        esac
 shift
 done
 
-#regex=“^machdep\.cpu\.thread_count:\s*([0-9]*)\n$”
-#string=“$(sysctl -a | grep machdep.cpu.thread_count)”
-
-threads=$(sysctl -a | grep machdep.cpu.thread_count)
-regex="machdep.cpu.thread_count: ([0-9]*)"
-
 cores="1"
 if [ $j == "0" ]; then
-	if [ "$(uname -s)" == "Darwin" ]; then
-		[[ $threads =~ $regex ]]
-		cores=${BASH_REMATCH[1]}
-	else
-		cores=$(grep -c ^processor /proc/cpuinfo) 
-	fi
+        if [ "$(uname -s)" == "Darwin" ]; then
+                threads=$(sysctl -a | grep machdep.cpu.thread_count)
+                regex="machdep.cpu.thread_count: ([0-9]*)"
+                [[ $threads =~ $regex ]]
+                cores=${BASH_REMATCH[1]}
+        else
+                cores=$(grep -c ^processor /proc/cpuinfo) 
+        fi
 else
-	cores=$j
+        cores=$j
 fi
 
 if [ ! -d "build" ]; then
@@ -73,12 +69,12 @@ fi
 
 cd "build"
 if [ $compiler == "clang" ]; then
-	cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_BUILD_TYPE=$build -DFORMULA_SIZE:STRING=$size ..
+        cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_BUILD_TYPE=$build -DFORMULA_SIZE:STRING=$size ..
 elif [ $compiler == "gcc" ]; then
-	cmake -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DCMAKE_BUILD_TYPE=$build -DFORMULA_SIZE:STRING=$size ..
+        cmake -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DCMAKE_BUILD_TYPE=$build -DFORMULA_SIZE:STRING=$size ..
 else
-	echo "ERROR: Unsupported compiler $1"
-	exit 1
+        echo "ERROR: Unsupported compiler $1"
+        exit 1
 fi
 make -j $cores
 cd ..
