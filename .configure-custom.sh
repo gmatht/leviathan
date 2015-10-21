a2471fad093bea52a488085966173ef688c0d3a0
Nicola Gigante <nicola.gigante@gmail.com>
2015-10-21 15:48:00 +0200

    Added --debug and --release options to the configure script and centralized the C++11 option

--- .configure-custom.sh
+++ .configure-custom.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#
+# This file is sourced by the configure script, 
+# it's useless to call it directly
+#
+
 if [ "x$TRAVIS_CLANG_VERSION" != "x" ]; then
 
   if [ "$CC" = "clang" ]; then
