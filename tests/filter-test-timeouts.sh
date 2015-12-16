96954dd7158ff0658f1fc37a4cbfacbfb4373844
Nicola Gigante <nicola.gigante@gmail.com>
2015-12-16 19:51:56 +0100

    StarExec test suite

---
+++ tests/filter-test-timeouts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while IFS=, read -r -a LINE; do
+  if gtimeout --foreground -s KILL 5s \
+    ./bin/checker --parsable --verbosity 0 ${LINE[0]}
+  then
+    echo ${LINE[0]} ${LINE[8]}
+  fi
+done 
