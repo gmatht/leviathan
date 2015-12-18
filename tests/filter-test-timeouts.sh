fbb884f502930ba81f50600f65c0f93c5cf3c8d6
Nicola Gigante <nicola.gigante@gmail.com>
2015-12-18 13:10:17 +0100

    Revised tests index

--- tests/filter-test-timeouts.sh
+++ tests/filter-test-timeouts.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+die() {
+  echo \
+This script must be executed from the root directory of leviathan\'s source \
+tree. 1>&2
+  exit 1
+}
+
+timeout() {
+  if [ "$(uname)" = "Darwin" ]; then
+    gtimeout $@
+  else
+    timeout $@
+  fi
+}
+
+test -d .git || die
+
 while IFS=, read -r -a LINE; do
   [ ${LINE[8]} != "UNK" ] || continue
   
-  if gtimeout --foreground -s KILL 5s \
+  if timeout --foreground -s KILL 3s \
     ./bin/checker --parsable --verbosity 0 ${LINE[0]}
   then
-    echo ${LINE[0]} ${LINE[8]}
+    echo ${LINE[0]} ${LINE[8]} | sed 's/UNS/UNSAT/'
   fi
 done 
