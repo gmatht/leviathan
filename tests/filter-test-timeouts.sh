0fdc489c99273f61d1d11abceaf15af390e6134d
Nicola Gigante <nicola.gigante@gmail.com>
2015-12-19 15:44:16 +0100

    Fix to test filter script to accept the CSV header as is

--- tests/filter-test-timeouts.sh
+++ tests/filter-test-timeouts.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#
+# This script processes a CSV file obtained by StarExec 'job information' 
+# command and extract all and only the benchmarks that complete in at most 3 
+# seconds.
+#
+# The output file format is suitable to be parsed by CMakeLists.txt when
+# configuring CTest, i.e. each line is a test file followed by "SAT" or 
+# "UNSAT", separated by a semicolon
+#
+
+
 die() {
   echo \
 This script must be executed from the root directory of leviathan\'s source \
@@ -7,25 +18,36 @@ tree. 1>&2
   exit 1
 }
 
-timeout() {
-  if [ "$(uname)" = "Darwin" ]; then
-    gtimeout $@
-  else
-    timeout $@
-  fi
-}
+# Wrapper for the timeout command
+mytimeout()
+{  
+  # The command has a different name when installed by Homebrew
+  command=timeout
+  [ "$(uname)" = "Darwin" ] && command=gtimeout
 
-test -d .git || die
-
-while IFS=", " read -r -a LINE; do
-  [ ${LINE[8]} != "UNK" ] || continue
-  
-  timeout --foreground -s KILL 3s \
-    ./bin/checker --verbosity 0 ${LINE[0]}
+  $command --foreground -s KILL 3s $@
 
   # timeout returns 124 or 137 if it had to kill the process
   # Yes I love Unix...
-  if [ $? -ne 124 -a $? -ne 137 ]; then
-    echo "${LINE[0]};${LINE[8]}" | sed 's/UNS/UNSAT/'
+  [ $? -ne 124 -a $? -ne 137 ] && return 0 || return 1
+}
+
+# Check that we run from the topmost source dir
+[ -d .git ] || die
+
+while IFS=", " read -r -a LINE; do
+  # Parse the line
+  filename=${LINE[0]}
+  result=${LINE[8]}
+
+  # Discard CSV header
+  [ $filename != "benchmark" ] || continue
+
+  # Discard entries with unknown result
+  [ $result != "UNK" ] || continue
+
+  # Launch checker
+  if mytimeout ./bin/checker --verbosity 0 "$filename"; then
+    echo "$filename;$result" | sed 's/UNS/UNSAT/'
   fi
 done 
