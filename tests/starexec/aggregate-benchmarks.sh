183338b2c663ef09244b439a9595b37da4ab830f
Nicola Gigante <nicola.gigante@gmail.com>
2016-01-25 19:40:11 +0100

    StarExec data and aggregation script

--- tests/starexec/aggregate-benchmarks.sh
+++ tests/starexec/aggregate-benchmarks.sh
@@ -1,52 +1,34 @@
 #!/bin/bash
 
-columns() {
-  fields=$1
-  file=$2
+[ "$1" != "" ] || (echo "Give me the args, bro" && exit 1)
 
-  cat $file | sed '1 d' | cut -d , -f $fields
+solvers="aalta trp++ ls4 Leviathan-06d4951 NuSMV-2.6.0,BMC NuSMV-2.6.0,BDD pltl,graph pltl,tree"
+
+formulae=$(find acacia alaska/lift schuppan trp -depth 1; find rozier anzu -depth 2)
+
+filt() {
+  cat $1 | grep $formula | grep $solver | grep $2 | wc -l
 }
 
-if [ $# -lt 2 ]; then
-   cat <<LOL 1>&2
-Usage:
-   aggregate-benchmarks.sh <output file> <file1> [<file2> <file3> ...]
-LOL
-  exit 1
-fi
+memory() {
+  cat $1 | grep $formula | grep $solver | grep -v timeout | grep -v memout | \
+    cut -d',' -f 7 | awk '{s+=$1;n+=1} END {if (n == 0) {print "-"} else {print s/n}}'
+}
 
-output=$1
-
-shift
-
-echo "benchmark" > $output
-columns 1 $1 >> $output
-
-while [ ! -z "$1" ]; do
-  
-  file=$1
-  shift
-
-  [ $output != $file ] || continue
-
-  echo "Aggregating $file..."
-
-  solver=$(basename "$file" .csv)
-  
-  header="$solver-status,$solver-time,$solver-memory"
-  
-  paste -d , $output <(echo $header; columns 4,5,7 $file) > $output.tmp
-
-  mv $output.tmp $output
-
+(
+echo -n "formula "
+for solver in $solvers; do
+  #echo -n "$solver-memouts $solver-timeouts "
+  echo -n "$solver-memory "
 done
+echo
 
-# Convert to space-separated table
-cat $output                       \
-  | sed "s/timeout (.*)/timeout/" \
-  | column -t -s ,                \
-  > $output.temp
-
-mv $output.temp $output
-
-
+for formula in $formulae; do
+  echo -n "$formula "
+  for solver in $solvers; do
+    #echo -n "$(filt $1 memout) $(filt $1 timeout) "
+    echo -n "$(memory $1) "
+  done
+  echo
+done
+) | column -t
