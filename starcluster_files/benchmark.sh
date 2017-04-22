f85807cf6b3960d141402893a1fa5b618ab70200
John McCabe-Dansted <gmatht@gmail.com>
2017-04-22 21:30:40 +0800

    Split benchmark sets up by time taken

--- starcluster_files/benchmark.sh
+++ starcluster_files/benchmark.sh
@@ -1,12 +1,12 @@
 git diff .. > ~/store/diff.txt
-for L in U S H
+for L in U0 U1 U2 U3 S0 S1 S2 S3 S4 H
 do
 cat ../tests/lists/$L | while read t f
 #f in `find ~/leviathan/tests/rozier/ | grep pltl\$`
  do echo --- $t $f
  i=$((i+1))
+ NAME="$L"_`printf %4d  $1 | tr \  0`
  time -p timeout 1000 bash parallel.sh "`cat ../tests/$f`" $L$i
  bash makelog.sh "`cat ../tests/$f`" $L$i "$f"
- done 
-done 2>&1 | tee ~/store/summary.txt
-
+ done 2>&1 | tee ~/store/summary_$L.txt
+done 
--- starcluster_files/make_lists.sh
+++ starcluster_files/make_lists.sh
@@ -2,4 +2,13 @@
 < benchmark.txt sed s/----/\#/ | tr '\n#' ' \n' > benchmark_oneline.txt
 sed 's/.* user //' < benchmark_oneline.txt | grep is.sat | sed 's/ .* tests./\t/' | grep .. | sort -n > tests/lists/S
 sed 's/.* user //' < benchmark_oneline.txt | grep is.unsat | sed 's/ .* tests./\t/' | grep .. | sort -n > tests/lists/U
-grep -v satisfiable  benchmark_oneline.txt | grep .. | sed 's/^ .*tests./?\t/' | shuf > tests/lists/H
+cd tests/lists/
+for t in S U
+do
+	< $t grep '^0[.][1-9]' > "$t"0
+	< $t grep '^[1-9][.]' > "$t"1
+	< $t grep '^[1-9].[.]' > "$t"2
+	< $t grep '^[1-9]..[.]' > "$t"3
+done
+	
+#grep -v satisfiable  benchmark_oneline.txt | grep .. | sed 's/^ .*tests./?\t/' | shuf > tests/lists/H
