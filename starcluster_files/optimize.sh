d9effd7c8dadde9405eebf36a6f6a43bfffeb41a
John McCabe-Dansted <gmatht@gmail.com>
2017-04-25 23:31:30 +0800

    Find depth, nJOB that provides shortest total time

---
+++ starcluster_files/optimize.sh
@@ -0,0 +1,23 @@
+#DEPTH=17; gcc parse_clock.cpp; for f in store/compute_serial/U[123]_*; do echo $f; zcat $f | PC_FLOAT=y ./a.out $DEPTH:719 | average.sh; zcat $f | PC_FLOAT=y ./a.out $DEPTH:720 | average.sh 2> /dev/null; zcat $f | PC_FLOAT=y ./a.out $DEPTH:718 | average.sh 2> /dev/null; done
+gcc parse_clock.cpp -O2 -o parse_clock
+DEPTHS="`seq 1 20`"
+zcat $(ls store/compute_serial/U?_*gz) > /tmp/zcat
+(
+for DEPTH in $DEPTHS; do echo -ne "\t$DEPTH"; done; echo
+for JOB in `seq 1 720`; do
+echo  -n "$JOB:" 
+for DEPTH in $DEPTHS; do
+	echo -ne '\t'
+	#printf "%7s" `< /tmp/zcat PC_FLOAT=y ./parse_clock $DEPTH:$JOB | average.sh 2> /dev/null | cut -f6 | tr -d '\n' | sed s/.$//`
+	#printf "%7s" `< /tmp/zcat PC_FLOAT=y ./parse_clock $DEPTH:$JOB | average.sh 2> /dev/null | cut -f6 | tr -d '\n' | sed s/.$//`
+	echo -n `< /tmp/zcat PC_SUM=y ./parse_clock $DEPTH:$JOB`
+done
+echo
+done
+
+for DEPTH in $DEPTHS; do echo -ne "\t$DEPTH"; done; echo
+) | tee optimize.txt
+
+echo 2 | perl -pe 'sub even {$j=1;$k=$i_[0];while(($k%2==0) and ($k>0)){$k=$k/2;$j++} return $j } ;s/([0-9]*)/$1,even($1)/e' < optimize.txt > optimize_witheveness.sh
+#paste <(for f in store/compute_serial/U[123]_*.gz; do  zcat $f | PC_FLOAT=y PC_RR=Y ./parse_clock 17:4 | sed s/...$// | tr '\n' '\t'; echo; done) <(yes '|' |head -n99) <(grep 'user$' < store/summary/compute2x2_rr_summary.txt) | less | grep ^[0-9] | sed s/user/\\t/g | tee store/summary/compute2x2_rr_summary_vs_estimate.txt
+#paste <(for f in store/compute_serial/U[123]_*.gz; do  echo `zcat $f | PC_FLOAT=y PC_RR=Y ./parse_clock 17:4`; done) <(grep 'user$' < store/summary/compute2x2_rr_summary.txt) | less > store/summary/compute2x2_rr_summary_vs_estimate.txt
--- starcluster_files/parse_clock.cpp
+++ starcluster_files/parse_clock.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int use_float=getenv("PC_FLOAT")>0; 
 int round_robin=getenv("PC_RR")>0; 
+int pc_max=getenv("PC_MAX")>0; 
+int pc_sum=getenv("PC_SUM")>0; 
 
 unsigned int num_jobs=0;
 
@@ -38,23 +41,28 @@ int main(int argc, char *argv[]) {
 	sscanf(argv[1],"%u:%u",&split_depth,&num_jobs);
 	unsigned int depth=0, l_depth=0;
 	unsigned long long time=0, l_time=0;
+	unsigned long long max=0;
+	double sum;
 	
 	int n_read;
 
 	job_clock=(unsigned long long*)calloc(num_jobs,sizeof(unsigned long long));
 
 	while ((n_read = scanf("^%u:%llu\n", &depth, &time)) >= 0) {
+
+		// swallow lines not of the right format
 		if (n_read < 2) {
 			scanf("%*[^\n]");
 			scanf("\n");
 			//printf("----\n");
 			continue;
 		}
-		//printf("%d\t%u\t%llu\n",n_read,depth,time);
+
 		if (depth <= split_depth) {
 			if (l_depth == split_depth) {
 				long long d_time = (time-l_time);
 				if (num_jobs) {
+					if (d_time > max) max = d_time;
 					if (round_robin) {
 						job_clock[job++]+=d_time;
 						job %= num_jobs;
@@ -68,13 +76,38 @@ int main(int argc, char *argv[]) {
 			l_depth=depth;
 			l_time =time ;
 		}
+
+		//end of one formulas benchmark
+		if (depth==0) {
+			if (num_jobs)
+				for (unsigned int i=0;i<num_jobs;i++) {
+					if (job_clock[i] > max) max = job_clock[i];
+					if (!(pc_max||pc_sum)) {
+						print_time(job_clock[i]);
+					}
+			}
+			if (pc_max) 
+				print_time(max);
+			sum+=(double)max;
+
+			job=0; depth=0; l_depth=0; time=0; l_time=0; max=0;
+			memset(job_clock, 0, num_jobs*sizeof(unsigned long long));
+			continue;
+		}
+
 	}
 
-	if (num_jobs)
-		for (unsigned int i=0;i<num_jobs;i++) 
-			print_time(job_clock[i]);
-
 	free(job_clock);
+
+	if (depth) {
+		fprintf (stderr, "Missing '^0' at end of input?");
+		return 1;
+	} else {
+		if (pc_sum)
+                	printf("\t%4.2lf\n",sum/(1000*1000));
+	}
+
+
 
 	return 0;
 }
