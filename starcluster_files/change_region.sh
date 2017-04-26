e9cb6b26c6f27c4323a483fae1e67f4f1ba5b686
John McCabe-Dansted <gmatht@gmail.com>
2017-04-26 16:51:51 +0800

    Fix prev commit

--- starcluster_files/change_region.sh
+++ starcluster_files/change_region.sh
@@ -4,6 +4,7 @@ ami=$2
 if [ -z "$2" ]
 then 
 	echo usage: $0 region ami
+	echo $0 ca-central-1 ami-b3d965d7
 	firefox  https://simonpbriggs.co.uk/amazonec2/
 	exit 1
 fi
@@ -14,11 +15,16 @@ NODE_IMAGE_ID=ami-8fcc75ec
 NEW_HOME=~/.home.$region
 mkdir -p $NEW_HOME
 
-ln -s $NEW_HOME/.ssh ~/.ssh 
+ln -s ~/.ssh $NEW_HOME/.ssh 
 mkdir $NEW_HOME/.starcluster
+
 < ~/.starcluster/config sed "s/$AWS_REGION_NAME/$region/g
 s/$NODE_IMAGE_ID/$ami/g" > $NEW_HOME/.starcluster/config 
 
+ln ~/.gitconfig $NEW_HOME/.gitconfig
+
+#[ -e ~/.ssh/mykey-$region.rsa ] || HOME=$NEW_HOME starcluster ck $region -o ~/.ssh/mykey-$region.rsa
+[ -e ~/.ssh/mykey-$region.rsa ] || HOME=$NEW_HOME starcluster ck mykey -o ~/.ssh/mykey-$region.rsa
 HOME=$NEW_HOME bash
 
 exit
